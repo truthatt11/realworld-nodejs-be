05e7b4301ba1720dd8a5ffac38b63f727d205169
Chris Yang <yjunhao1111@gmail.com>
2022-07-24 21:08:59 -0400

    Add nodemon to project

--- src/index.ts
+++ src/index.ts
@@ -3,69 +3,69 @@ import typeDefs from './schema';
 import resolvers from './resolvers';
 import LocalAPI from './datasources/local-api';
 
-const mocks = {
-    Query: () => ({
-        getUser: () => {
-            console.log('[GET_USER]');
-            return {
-                code: 200,
-                success: true,
-                message: "",
-                user: {
-                    email: "getuser@graphql.com",
-                    token: "111111",
-                    username: "getuser",
-                    bio: "hello graphql 1",
-                    image: "123.jpg"
-                },
-            };
-        }
-    }),
-    Mutation: () => ({
-        loginUser: (email: string, password: string) => {
-            return {
-                code: 200,
-                success: true,
-                message: "",
-                user: {
-                    email: "loginuser@graphql.com",
-                    token: "222222",
-                    username: "loginuser",
-                    bio: "hello graphql 2",
-                    image: "234.jpg"
-                }
-            };
-        },
-        createUser: (username: string, email: string, password: string) => {
-            return {
-                code: 200,
-                success: true,
-                message: "",
-                user: {
-                    email: "createuser@graphql.com",
-                    token: "333333",
-                    username,
-                    bio: "hello createUser",
-                    image: "345.jpg"
-                }
-            };
-        },
-        updateUser: (email: string, token: string, username: string, bio: string, image: string) => {
-            return {
-                code: 200,
-                success: true,
-                message: "",
-                user: {
-                    email: "updateuser@graphql.com",
-                    token: "444444",
-                    username,
-                    bio: "hello updateUser",
-                    image: "456.jpg",
-                }
-            };
-        },
-    }),
-};
+// const mocks = {
+//     Query: () => ({
+//         getUser: () => {
+//             console.log('[GET_USER]');
+//             return {
+//                 code: 200,
+//                 success: true,
+//                 message: "",
+//                 user: {
+//                     email: "getuser@graphql.com",
+//                     token: "111111",
+//                     username: "getuser",
+//                     bio: "hello graphql 1",
+//                     image: "123.jpg"
+//                 },
+//             };
+//         }
+//     }),
+//     Mutation: () => ({
+//         loginUser: (email: string, password: string) => {
+//             return {
+//                 code: 200,
+//                 success: true,
+//                 message: "",
+//                 user: {
+//                     email: "loginuser@graphql.com",
+//                     token: "222222",
+//                     username: "loginuser",
+//                     bio: "hello graphql 2",
+//                     image: "234.jpg"
+//                 }
+//             };
+//         },
+//         createUser: (username: string, email: string, password: string) => {
+//             return {
+//                 code: 200,
+//                 success: true,
+//                 message: "",
+//                 user: {
+//                     email: "createuser@graphql.com",
+//                     token: "333333",
+//                     username,
+//                     bio: "hello createUser",
+//                     image: "345.jpg"
+//                 }
+//             };
+//         },
+//         updateUser: (email: string, token: string, username: string, bio: string, image: string) => {
+//             return {
+//                 code: 200,
+//                 success: true,
+//                 message: "",
+//                 user: {
+//                     email: "updateuser@graphql.com",
+//                     token: "444444",
+//                     username,
+//                     bio: "hello updateUser",
+//                     image: "456.jpg",
+//                 }
+//             };
+//         },
+//     }),
+// };
 
 const server = new ApolloServer({
     typeDefs,
